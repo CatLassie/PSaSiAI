0b1acc566c6a2673e14966b47af220d1fbebbd37
Peter_Ryabchenko <ryabchenko.peter@gmail.com>
2019-06-01 21:13:14 +0200

    - added command line support for instance number and construction heuristic type

--- ex2_vertex_cover/program/src/main/Main.java
+++ ex2_vertex_cover/program/src/main/Main.java
@@ -12,14 +12,25 @@ import parser.KPMPInstance;
 public class Main {
 
 	public static void main(String[] args) {	
-		String instanceN = "01";
-		String readPath = "D:/uni/2019SS/PSaSiAI/PSaSiAIrepo/ex2_vertex_cover/heuropttechinstances/instance-"+instanceN+".txt";
+		String instanceN = args[0];
+		String constrType = args[1];
+		
+		System.out.println("\nMain.main()");
+		System.out.println("command line args:");
+		System.out.println("instance number: "+instanceN+"");
+		System.out.println("contruction heuritic type: "+constrType+"\n");
+		
+		// path for the bin
+		// String readPath = "../../heuropttechinstances/instance-"+instanceN+".txt";
+		String readPath = "file:/../../heuropttechinstances/instance-"+instanceN+".txt";
 		
 		try {
 			KPMPInstance k = KPMPInstance.readInstance(readPath);
-			//System.out.println(k.getNumVertices());
+						
+			IConstruction constr = constrType.equals("random") ? new Randomized()
+					: (constrType.equals("greedy") ? new Greedy()
+							: (constrType.equals("wrost") ? new Worst() : new Worst()));
 			
-			IConstruction constr = new Randomized(); // Worst / Randomized / Greedy
 			Solution initialSolution = constr.generateSolution(k);
 			
 			System.out.println(initialSolution);
